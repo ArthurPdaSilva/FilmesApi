462f985630b4e8c15a063b04b0c4acd0942a2f5a
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2023-06-10 19:44:59 -0300

    feat: adição de gets

--- FilmesApi/Controllers/FilmesController.cs
+++ FilmesApi/Controllers/FilmesController.cs
@@ -7,15 +7,29 @@ namespace FilmesApi.Controllers;
 [Route("[controller]")]
 public class FilmesController : ControllerBase
 {
-    private static List<Filme> filmes = new List<Filme>();
+    private static List<Filme> _filmes = new List<Filme>();
+    private static int _id = 0;
 
     [HttpPost]
     public void AdicionarFilme([FromBody] Filme filme)
     {
-        filmes.Add(filme);
+        filme.Id = _id++;
+        _filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
 
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuparFilmes()
+    {
+        return _filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuparFilmePorId(int id)
+    {
+        return _filmes.FirstOrDefault(filme => filme.Id == id);
     }
 
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,6 +4,7 @@ namespace FilmesApi.Models;
 
 public class Filme
 {
+    public int Id { get; set; }
     [Required(ErrorMessage = "O título do filme é obrigatório")]
     public string Titulo { get; set; }
     [Required(ErrorMessage = "O gênero do filme é obrigatório")]
@@ -12,4 +13,5 @@ public class Filme
     [Required]
     [Range(70, 600, ErrorMessage = "A duração deve ter entre 70 e 600 minutos")]
     public int Duracao { get; set; }
+    
 }
