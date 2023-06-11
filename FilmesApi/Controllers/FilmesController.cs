4a19ac423118ce2a9b80dfed0158bd7d9c6eeaa4
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2023-06-11 10:08:49 -0300

    feat: adição de documentação com swagger

--- FilmesApi/Controllers/FilmesController.cs
+++ FilmesApi/Controllers/FilmesController.cs
@@ -21,7 +21,15 @@ public class FilmesController : ControllerBase
         _mapper = mapper;
     }
 
+
+    /// <summary>
+    /// Adiciona um filme ao banco de dados
+    /// </summary>
+    /// <param name="filmeDTO">Objeto com os campos necessários para criação de um filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso inserção seja feita com sucesso</response>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
     public IActionResult AdicionarFilme([FromBody] CreateFilmeDTO filmeDTO)
     {
         Filme filme = _mapper.Map<Filme>(filmeDTO);
